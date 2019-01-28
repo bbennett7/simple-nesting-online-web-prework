e60a1d729be51c15c77f0e36b12abaa56e87d674
Bryn Bennett <bryn.bennett@gmail.com>
2019-01-28 04:03:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -125,6 +125,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages]
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 
+  return programmer_hash
 end
